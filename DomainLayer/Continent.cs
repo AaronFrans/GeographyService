52fcaf4f5d29aebc1f0627704b345fbc1270d660
AaronFrans <azdefatrhtws@gmail.com>
2020-12-04 11:20:25 +0100

    Add project files.

---
+++ DomainLayer/Continent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DomainLayer
+{
+    public class Continent
+    {
+        public int Id { get; set;}
+        public string Name { get; private set; }
+        public int Population { get; private set; }
+    }
+}
